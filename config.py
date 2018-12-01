bd28f26f3aed510a369e4682d38bd6e1e494e511
progressforward <43518314+progressforward@users.noreply.github.com>
2018-12-01 19:38:07 +0800

    config update

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 465
-    #MAIL_USE_TLS = True
+    MAIL_USE_TLS = True
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
@@ -35,6 +35,25 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+        
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USE_TLS', None):
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost = (cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr = [cls.FLASKY_ADMIN],
+            subject = cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Appalication Error',
+            credentials = credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 config = {
     'development': DevelopmentConfig,
